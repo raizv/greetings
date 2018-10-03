18460e143ae2560bc215eba37564d957c74fda9f
Anton Raizvikh <raizvih@gmail.com>
2018-10-03 07:40:20 -0700

    fix newrelic notify script

--- openshift/run-newrelic-notify.sh
+++ openshift/run-newrelic-notify.sh
@@ -11,7 +11,7 @@ VERSION=${3:-latest}
 APP_ID=`curl -s -X GET "https://api.newrelic.com/v2/applications.json" \
   -H "X-Api-Key:${NEW_RELIC_API_KEY}" \
   -d "filter[name]=${CONTAINER_NAME}+(${ENVIRONMENT})" \
-| jq ".applications[] | select(.name == \"${CONTAINER_NAME} (${ENVIRONMENT})\") | .id"`
+| jq ".applications[] | select(.name == \"${CONTAINER_NAME} ${ENVIRONMENT}\") | .id"`
 
 if ! test ${APP_ID} = "null"
 then
