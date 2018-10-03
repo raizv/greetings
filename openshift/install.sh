deaad4fe77818794391753fc77bc576c54a2df81
Anton Raizvikh <raizvih@gmail.com>
2018-10-03 11:41:49 -0700

    add build config to openshift template

---
+++ openshift/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -o nounset -o errexit
+cd `dirname $0`
+
+# Installs the build pipeline for a given branch (default: master) in your currently selected OpenShift project
+# See: README.md
+
+APP_NAME=greetings
+BRANCH=${1:-master}
+
+# Github SSH Key
+oc create secret generic github-secret --from-file=ssh-privatekey=id_rsa --dry-run -o yaml | oc apply -f -
+
+# Newrelic License
+oc create secret generic newrelic-license-secret \
+    --from-literal=newrelic-license="newrelic-license" \
+    --dry-run -o yaml | oc apply -f -
+
+# Newrelic API Key
+oc create secret generic newrelic-api-secret \
+    --from-literal=newrelic-key="newrelic-key" \
+    --dry-run -o yaml | oc apply -f -
+
+# Apply and execute the OpenShift template
+oc apply -f openshift-template.yml
+oc process ${APP_NAME}-pipeline BRANCH=${BRANCH} | oc apply -f -
+oc start-build ${APP_NAME}-pipeline
