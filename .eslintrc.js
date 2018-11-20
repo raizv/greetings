4f6bf74ba45c9864ed9bdadcb335641c23f15da6
Anton Raizvikh <raizvih@gmail.com>
2018-11-20 12:48:15 -0800

    apply security updates, update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "comma-dangle": 0,
-        "semi": 0
+        "semi": 0,
+        "import/no-dynamic-require": 0,
+        "no-console": 0
     }
 }
