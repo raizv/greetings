e6427121b194de6088d445141c1412d7fa021085
Anton Raizvikh <raizvih@gmail.com>
2019-11-06 11:36:18 -0800

    add cloudbuild config

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,15 +1,15 @@
 
 variable "region" {
-    description = "Default AWS region"
-    default = "us-west-2"
+  description = "Default AWS region"
+  default     = "us-west-2"
 }
 
 variable "app_name" {
-    description = "Application name"
-    default = "greetings"
+  description = "Application name"
+  default     = "greetings"
 }
 
 variable "tf_state_name" {
-    description = "S3 bucket and DynamoDB table names to store terraform state"
-    default = "greetings-tf-state"
+  description = "S3 bucket and DynamoDB table names to store terraform state"
+  default     = "greetings-tf-state"
 }
