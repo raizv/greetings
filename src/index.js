e484775b44604b7008b75a2126dad57ca4272a2b
Anton Raizvikh <raizvih@gmail.com>
2019-11-19 10:06:20 -0800

    use newrelic and google trace in prod only

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-require('newrelic')
+const newrelic = require('newrelic')
+
+const traceAgent = require('@google-cloud/trace-agent').start({
+  ignoreUrls: ['/health'],
+  serviceContext: {
+    service: 'greetings'
+    // version: string
+    // minorVersion: string
+  }
+})
 
 if (process.env.NODE_ENV === 'prod') {
-  require('@google-cloud/trace-agent').start({
-    ignoreUrls: ['/health'],
-    serviceContext: {
-      service: 'greetings'
-      // version: string
-      // minorVersion: string
-    }
-  })
+  newrelic()
+  traceAgent()
 }
 
 const config = require('./config')
